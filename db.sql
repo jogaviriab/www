b9bfeb58e7941dc126b7954af11d39b1dc2f03af
leodavid0109 <leodavid0521@gmail.com>
2024-09-26 21:17:58 -0500

    Jodedera

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE escuderia(
-  codigo INT PRIMARY KEY,
+  codigo INT PRIMARY KEY CHECK (codigo >= 0),
   nombre VARCHAR(30) NOT NULL,
   paisOrigen VARCHAR(20) NOT NULL,
   sede VARCHAR(20) NOT NULL,
@@ -9,21 +9,22 @@ CREATE TABLE escuderia(
 
 
 CREATE TABLE piloto(
-    numPiloto INT PRIMARY KEY,
+    numPiloto INT PRIMARY KEY CHECK (numPiloto >= 0),
     nombre VARCHAR(30) NOT NULL,
     fechaNacimiento DATE NOT NULL,
     nacionalidad VARCHAR(20) NOT NULL,
     estiloCond VARCHAR(11) NOT NULL CHECK (estiloCond IN ('Agresivo', 'Conservador')),
     alias VARCHAR(20) NOT NULL,
     escuderia INT NOT NULL REFERENCES escuderia(codigo),
-    salario INT NOT NULL CHECK (salario > 0)
+    salario INT NOT NULL CHECK (salario >= 0)
 );
 
 
 CREATE TABLE prueba (
-  codigo INT PRIMARY KEY,
-  distancia INT NOT NULL,
+  codigo INT PRIMARY KEY CHECK (codigo >= 0),
+  distancia INT NOT NULL CHECK (distancia >= 0),
   fecha DATE NOT NULL,
   realizadaPor INT NOT NULL REFERENCES piloto(numPiloto),
-  analizadaPor INT REFERENCES piloto(numPiloto)
+  analizadaPor INT REFERENCES piloto(numPiloto),
+  CHECK (realizadaPor <> analizadaPor OR analizadaPor IS NULL)
 );
